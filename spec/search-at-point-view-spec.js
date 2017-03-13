837620c3f9e261ea793bbc2358a92278b68ed108
Guy Lyons <guylyons2@gmail.com>
2017-03-13 12:49:50 -0400

    init

---
+++ spec/search-at-point-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SearchAtPointView from '../lib/search-at-point-view';
+
+describe('SearchAtPointView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
